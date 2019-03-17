6871d4607681165d431ec89d16d8f38cb10c14ee
Tomek Kulczyński <kulczynski.t@gmail.com>
2019-03-17 21:58:29 +0100

    finished task zadanie01

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,19 @@
-//Twój kod
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, './public/zadanie01/')));
+
+app.post('/form', (req, res) => {
+    const { num1, num2 } = req.body;
+    const success = `<h1>${num2} jest dzielnikiem ${num1}</h1>`
+    const failure = `<h1>${num2} nie jest dzielnikiem ${num1}</h1>`
+
+    res.send(num1 % num2 !== 0 ? failure : success);
+  })
+
+app.listen(3000, () => {
+    console.log('Server running on :3000');
+});
